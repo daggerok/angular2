089f4642a3f5893f3d0b9b2adbcd0d696ff30000
Maksim Kostromin <daggerok@gmail.com>
2016-10-23 02:07:10 +0300

    add Subject

--- src/components/app/component.ts
+++ src/components/app/component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-// import 'rxjs/add/observable/interval'; // accesible from vendors
+import { Observable, Subject } from 'rxjs';
+// // accesible from vendors
+// import 'rxjs/add/observable/interval';
+// import 'rxjs/add/operator/map';
 
-import './style.styl';
+import './styles.styl';
 
 @Component({
   selector: 'app',
-  template: `<h4>we are up and running at least {{ clock | async }} seconds!</h4>`,
+  template: require('./template.html'),
 })
 export class AppComponent {
-  clock: Observable<number> = Observable.interval(1000);
+  click$: Subject<any> = new Subject();
+  uptime = Observable.interval(1000);
+  date = Observable.interval(1000)
+                    .map(() => new Date());
+  update: Observable<Date>;
+  constructor() {
+    // rx 1 approach:
+    Observable.fromEvent(this.onUptimeClick(), 'click');
+    // rx 2 approach:
+    this.update = this.click$.map(() => new Date());
+  }
+
+  // ng2 approach
+  public onUptimeClick(): any {
+
+  }
 }
