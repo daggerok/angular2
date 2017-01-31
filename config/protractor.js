379e9c8ab63aba08aea854c3169e8b5d22f8905d
Maksim Kostromin <daggerok@gmail.com>
2017-01-31 22:43:25 +0200

    update version

--- config/protractor.js
+++ config/protractor.js
@@ -1,37 +1,29 @@
-const SpecReporter = require('jasmine-spec-reporter');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const timeout = 36000;
 
 exports.config = {
-  allScriptsTimeout: 30000,
-
+  allScriptsTimeout: timeout,
   specs: [
     '../e2e/**/*.e2e-spec.ts',
   ],
-
   capabilities: {
     browserName: 'chrome',
   },
-
   directConnect: true,
   chromeOnly: true,
-
   baseUrl: 'http://localhost:8000/',
-
   framework: 'jasmine',
-
   jasmineNodeOpts: {
     showColors: true,
-    defaultTimeoutInterval: 30000,
+    defaultTimeoutInterval: timeout,
     print: function() {},
   },
-
   useAllAngular2AppRoots: true,
-
   beforeLaunch: function() {
     require('ts-node').register({
       project: './e2e',
     });
   },
-
   onPrepare: function() {
     jasmine.getEnv().addReporter(new SpecReporter());
   },
